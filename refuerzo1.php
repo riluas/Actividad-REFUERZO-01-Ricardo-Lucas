674cd57e1b30ac4fd2c6636fbbabeff1aa8af5fb
riluas <44229797+riluas@users.noreply.github.com>
2019-01-23 11:57:46 +0100

    Add files via upload

---
+++ refuerzo1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    echo "<ul>";
+      for ($i=1; $i <41 ; $i=$i+2) {
+        echo "<li>$i</li>";
+      }
+      echo "</ul>";
+    ?>
+  </body>
+</html>
